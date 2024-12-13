b541431c79bf5503e8ad026db8b36b6385471eb1
pyzahl <pyzahl@gmail.com>
2024-12-13 15:50:02 -0500

    adjusted ids

--- plug-ins/hard/rpspmc_pacpll.cpp
+++ plug-ins/hard/rpspmc_pacpll.cpp
@@ -1957,7 +1957,7 @@ void RPSPMC_Control::create_folder (){
 
 
 		//note:  VP_FEEDBACK_HOLD is only the mask, this bit in GVP_opt is set to ONE for FEEBACK=ON !! Ot is inveretd while vector generation ONLY.
-		bp->grid_add_check_button ("", bp->PYREMOTE_CHECK_HOOK_KEY_FUNC("Enable Zservo","dsp-gvp-Zservo",k), 1,
+		bp->grid_add_check_button ("", bp->PYREMOTE_CHECK_HOOK_KEY_FUNC("Enable Zservo","dsp-gvp-FB",k), 1,
                                                GCallback (callback_change_GVP_vpc_option_flags), this,
                                                GVP_opt[k], VP_FEEDBACK_HOLD);
                 EC_vpc_opt_list = g_slist_prepend( EC_vpc_opt_list, bp->button);
@@ -1965,7 +1965,7 @@ void RPSPMC_Control::create_folder (){
 
                 bp->set_configure_list_mode_on (); // ================ advanced section
 
-                bp->grid_add_check_button ("", bp->PYREMOTE_CHECK_HOOK_KEY_FUNC("Vector is a absolute reference (goto)","dsp-gvp-Set",k), 1,
+                bp->grid_add_check_button ("", bp->PYREMOTE_CHECK_HOOK_KEY_FUNC("Vector is a absolute reference (goto)","dsp-gvp-VS",k), 1,
                                            GCallback (callback_change_GVP_vpc_option_flags), this,
                                            GVP_opt[k], VP_INITIAL_SET_VEC);
                 EC_vpc_opt_list = g_slist_prepend( EC_vpc_opt_list, bp->button);
