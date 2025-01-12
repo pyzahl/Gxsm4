ae53c891fd5b57862af9dce40b194f87b5e4af62
Percy Zahl <pyzahl@gmail.com>
2025-01-11 23:32:28 -0500

    added readback rtl

---
+++ plug-ins/hard/RPSPMC/rtl/readback_configuration.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/11/2025 12:04:22 AM
+// Design Name: 
+// Module Name: readback_configuration
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module readback_configuration#(
+    /* module readback register addresses */
+    parameter readback_Z_reg_address    = 100001,
+    parameter readback_Bias_reg_address = 100002,
+    parameter readbackX_reg_address = 100999
+    )(
+    input aclk,
+    
+    input  [32-1:0] config_addr,
+    output [32-1:0] gpio_dataA,
+    output [32-1:0] gpio_dataB,
+
+    input wire [32-1:0] Z_GVP_mon,
+    input wire [32-1:0] Z_slope_mon,
+
+    input wire [32-1:0] Bias_mon,
+    input wire [32-1:0] Bias_GVP_mon,
+
+
+    input wire [32-1:0] rbXa,
+    input wire [32-1:0] rbXb
+);
+
+    reg [32-1:0]		reg_A=0;
+    reg [32-1:0]		reg_B=0;
+   
+        
+    assign gpio_dataA = reg_A;
+    assign gpio_dataB = reg_B;
+
+    always @(posedge aclk)
+    begin
+        // module readback configuration
+        case (config_addr)
+        readback_Z_reg_address:
+        begin
+            reg_A <= Z_GVP_mon;
+            reg_B <= Z_slope_mon;
+	    end
+
+        readback_Bias_reg_address:
+        begin
+            reg_A <= Bias_mon;
+            reg_B <= Bias_GVP_mon;
+	    end
+	  
+        readbackX_reg_address:
+	    begin
+            reg_A <= rbXa;
+            reg_B <= rbXb;
+	    end
+
+	default:
+	  begin
+            reg_A <= 1;
+            reg_B <= 2;
+	  end
+        endcase
+    end    
+    
+endmodule
