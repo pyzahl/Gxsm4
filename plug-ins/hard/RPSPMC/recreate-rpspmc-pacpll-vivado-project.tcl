533953fcf3b2e2e6aba32494c2acf1554b7693cf
pyzahl <pyzahl@gmail.com>
2025-01-13 16:26:21 -0500

    updated, testing recreate

--- plug-ins/hard/RPSPMC/recreate-rpspmc-pacpll-vivado-project.tcl
+++ plug-ins/hard/RPSPMC/recreate-rpspmc-pacpll-vivado-project.tcl
@@ -93,15 +93,6 @@
 # Check file required for this script exists
 proc checkRequiredFiles { origin_dir} {
   set status true
-  set files [list \
- "[file normalize "$origin_dir/project_RP-SPMC-PACPLL-Gxsm4/project_RP-SPMC-PACPLL-Gxsm4.srcs/utils_1/imports/synth_1/system_wrapper.dcp"]"\
-  ]
-  foreach ifile $files {
-    if { ![file isfile $ifile] } {
-      puts " Could not find local file $ifile "
-      set status false
-    }
-  }
 
   set files [list \
  "[file normalize "$origin_dir/rtl/cfg_to_axis.v"]"\
@@ -466,18 +457,11 @@ set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
 # Set 'utils_1' fileset object
 set obj [get_filesets utils_1]
 # Import local files from the original project
-set files [list \
- [file normalize "${origin_dir}/project_RP-SPMC-PACPLL-Gxsm4/project_RP-SPMC-PACPLL-Gxsm4.srcs/utils_1/imports/synth_1/system_wrapper.dcp" ]\
-]
-set imported_files [import_files -fileset utils_1 $files]
 
 # Set 'utils_1' fileset file properties for remote files
 # None
 
 # Set 'utils_1' fileset file properties for local files
-set file "synth_1/system_wrapper.dcp"
-set file_obj [get_files -of_objects [get_filesets utils_1] [list "*$file"]]
-set_property -name "netlist_only" -value "0" -objects $file_obj
 
 
 # Set 'utils_1' fileset properties
@@ -6787,7 +6771,6 @@ if { $obj != "" } {
 }
 set obj [get_runs synth_1]
 set_property -name "part" -value "xc7z020clg400-1" -objects $obj
-set_property -name "incremental_checkpoint" -value "$proj_dir/project_RP-SPMC-PACPLL-Gxsm4.srcs/utils_1/imports/synth_1/system_wrapper.dcp" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
 set_property -name "strategy" -value "Vivado Synthesis Defaults" -objects $obj
 
