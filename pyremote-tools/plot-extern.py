6c2f67181c3799414595df30f833900f4525c870
Percy Zahl <pyzahl@gmail.com>
2026-01-12 23:56:26 -0500

    added example for extern vpdata plot

--- pyremote-tools/plot-extern.py
+++ pyremote-tools/plot-extern.py
@@ -6,12 +6,13 @@ from scipy.interpolate import BSpline, make_interp_spline, make_smoothing_spline
 import math
 import time
 import matplotlib as mpl
-mpl.use('Agg')
-from matplotlib import ticker
-mpl.pyplot.close('all')
+
+import gxsm4process as gxsm4
+
+gxsm = gxsm4.gxsm_process()
 
 print ('*** Getting last vpdata set from master scan in ch=0 ***')
-columns, labels, units, xy = gxsm.get_probe_event(0,-1)  # ch=0, get last
+columns, labels, units, xy = gxsm.get_probe_event(1,-1)  # ch=2, get last VPdata set
 
 vpdata  = dict (zip (labels, columns[:,100:])) ## cut off points 0..100 (initial ramp points)
 vpunits = dict (zip (labels, units))
@@ -23,13 +24,13 @@ print ('Set Size       :', vpdata[labels[0]].shape)
 # Demo VPDATA plot
 plt.figure(figsize=(6, 4))
 plt.title('VP-Data Plot Demo')
-plt.plot (vpdata['Bias'], vpdata['22-LockIn-S-DC'], '-', alpha=0.8, label='LockIn-S-DC')
-plt.xlabel('Bias in ' + vpunits['Bias'])
-plt.ylabel('LockIn-DC in ' + vpunits['22-LockIn-S-DC'])
+plt.plot (vpdata['ZS-Topo'], vpdata['dFrequency'], '-', alpha=0.8, label='LockIn-S-DC')
+plt.xlabel('Z in ' + vpunits['ZS-Topo'])
+plt.ylabel('dF in ' + vpunits['dFrequency'])
 
 #plt.ylim (-1, 1)
 #plt.xlim (-0.1, 0.2)
 plt.legend()
 plt.grid()
-#plt.show()
-plt.savefig('/tmp/vpdata-plot-demo.png')
+plt.show()
+#plt.savefig('/tmp/vpdata-plot-demo.png')
