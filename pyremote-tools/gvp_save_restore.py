47121169c73c1757a70ecf85c356601ad10b4a6a
pyzahl <pyzahl@gmail.com>
2026-06-03 15:45:07 -0400

    Update GVP save restore and VP data helper

--- pyremote-tools/gvp_save_restore.py
+++ pyremote-tools/gvp_save_restore.py
@@ -7,23 +7,36 @@ gvp_vectors = []
 load = True
 
 #gxsm.get('dsp-gvp-du00')
+#print(gxsm.action('GETCHECK-dsp-gvp-FB00'))
 
 if load:
-	with open(filename, 'r') as file:
-        	gvp_vectors = json.load(file)
-	for v in gvp_vectors:
-		for key in v:
-			print (key, v[key])
-			gxsm.set(key, str(v[key]))
+        with open(filename, 'r') as file:
+                gvp_vectors = json.load(file)
+        for v in gvp_vectors:
+                for key in v:
+                        print (key, v[key])
+                        if key.startswith('GETCHECK-dsp-gvp'):
+                                if str(v[key]) == 'TRUE':
+                                        gxsm.action(key[3:])
+                                else:
+                                        gxsm.action('UN'+key[3:])
+                        else:
+                                gxsm.set(key, str(v[key]))
 
 else:
 
-	for eckey in gxsm.list_refnames ():
-		if eckey.startswith('dsp-gvp'):
-			print ('{} \t=>\t {}'.format(eckey, gxsm.get(eckey))) 
-			gvp_vectors.append ({eckey: gxsm.get(eckey)})
-			
-	#print (gvp_vectors)
+        # Read all vector comonents
+        for eckey in gxsm.list_refnames ():
+                if eckey.startswith('dsp-gvp'):
+                        print ('{} \t=>\t {}'.format(eckey, gxsm.get(eckey))) 
+                        gvp_vectors.append ({eckey: gxsm.get(eckey)})
+        # Read all vector check options (FB = Feedback/Control on/off in particular) results is a "TRUE" or "FALSE" string
+        for eckey in gxsm.list_actions ():
+                if eckey.startswith('GETCHECK-dsp-gvp'):
+                        print ('{} \t=>\t {}'.format(eckey, gxsm.action(eckey))) 
+                        gvp_vectors.append ({eckey: gxsm.action(eckey)})
 
-	with open(filename, 'w') as file:
-		json.dump(gvp_vectors, file)
+        #print (gvp_vectors)
+
+        with open(filename, 'w') as file:
+                json.dump(gvp_vectors, file)
